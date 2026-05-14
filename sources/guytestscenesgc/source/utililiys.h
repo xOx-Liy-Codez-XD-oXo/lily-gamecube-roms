a47e054d58a7c00d7c96f2a3f1b5d3919c7f8c30
liy ⛤ <lilycupcakemyers@gmail.com>
2026-05-14 14:44:03 -0700

    Break code

--- sources/guytestscenesgc/source/utililiys.h
+++ sources/guytestscenesgc/source/utililiys.h
@@ -3,8 +3,10 @@
 
 void * LilyCoolMalloc(uint size) {
 	int lala = (int)malloc(size + 31);
-	lala += lala % 32;
-	printf("Lily cool malloc to the rescue!!! %d bytes at 0x%x\n", size, lala); 
+	//lala += lala % 32;
+	lala += 31;
+	lala &= ~31;
+	//printf("Lily cool malloc to the rescue!!! %d bytes at 0x%x\n", size, lala); 
 	return (void *)lala;
 }
 
